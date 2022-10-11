eff10cd84ec3416cb3226f8f0ac8a2c1ff996a0a
Anatoliy <tolyanichworld@gmail.com>
2022-10-11 08:58:51 +0200

    Removed unnecessary stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 const express = require('express')
 require('./db/mongoose')
-const maintenance = require('./middlewares/maintenance')
-const Task = require('./models/Task')
-
 
 const userRouter = require('./routers/user.js')
 const taskRouter = require('./routers/task.js')
